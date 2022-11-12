51e599590e4001a080806e4a11a26661752baa02
robinmattern <robin.mattern@gmail.com>
2022-11-12 16:05:21 -0500

    .(21112.03 | Add RSS Info Var Set

--- ._2/JPTs/RSS/Info/RSS22-Info.sh
+++ ._2/JPTs/RSS/Info/RSS22-Info.sh
@@ -2,7 +2,8 @@
 #   RSS Info (Prod Copy)
 
 #       RSS_Scr=RSS22-Info_v0.7.80923.sh
-        RSS_Scr=RSS22-Info_p0.08.81014.sh
+#       RSS_Scr=RSS22-Info_p0.08.81014.sh
+        RSS_Scr=RSS22-Info_p0.08.21112.sh
 
         RSS_Dir=$( dirname "$0" )
         RSS22_Info=$RSS_Dir/$RSS_Scr
