03b7e8476c5110430c7cbfdc0f9bb0d7d791dc03
robinmattern <robin.mattern@gmail.com>
2022-12-03 13:48:10 -0500

    .(21203.01 | Exclude ".git" and node_modules

--- ._2/JPTs/RSS/FileList/RSS21_FileList_p1.07.sh
+++ ._2/JPTs/RSS/FileList/RSS21_FileList_p1.07.sh
@@ -3,14 +3,23 @@
 ##=========+====================+================================================+
 ##RD         FileList           | RSS File Lister aka RDir
 ##RFILE    +====================+=======+=================+======+===============+
-##FD   RSS21-FileList.sh        |   9479| 10/08/18  1:48a |   136| v1.5.81008.01
-##FD   RSS21_FileList.sh        |  12243| 11/14/22  8:05p |   179| v1.07.21114.2005
-##FD   RSS21_FileList.sh        |  12902| 11/17/22 12:01p |   184| v1.07.21117.1201
+##FD   RSS21-FileList.sh        |   9479| 10/08/18  1:48|   136| v1.5.81008.01
+##FD   RSS21_FileList.sh        |  12243| 11/14/22  8:05|   179| v1.07.21114.2005
+##FD   RSS21_FileList.sh        |  14263| 12/03/22 13:31|   188| p1.07-21203.1331
 ##DESC     .--------------------+-------+-----------------+------+---------------+
+#            List files similar to Windows dir.  Also use switches
 #
+#              rdir {Dir} {Filesearch} (not {Dir}/{Filesearch})
+#
+#              -r #  Recursively Down n Levels
+#              -s #  Sort by 1) Size, 2) Date, or 3) Name
+#              -s #r Sort in reverse order: highest at top
+#              -d #  Saved n Previous Days back
+#              -n #  Saved n Previous Months back
+#              -x "" Exclude files.  Defaults to node_modules and .git
 #
 ##LIC      .--------------------+----------------------------------------------+
-#            Copyright (c) 2018 SicommNet-JPT * Released under
+#            Copyright (c) 2018-2022 JScriptWare * Released under
 #            MIT License: http://www.opensource.org/licenses/mit-license.php
 ##FNCS     .--------------------+-------+-------------------+------+-----------+
 #
@@ -31,51 +40,48 @@
 # .(11010.01 10/10/21 RAM  8:05p| Add -i option, defaults to /node-modules$/
 # .(21114.07 11/13/22 RAM  8:05p| Change Headings
 # .(21117.02 11/17/22 RAM  9:45p| Get rid of ".40.3971848000  ."
+# .(21203.01 12/03/22 RAM 11:30a| Exclude only ".git"
 
 ##PRGM     +====================+===============================================+
 ##ID 69.600. Main               |
 ##SRCE     +====================+===============================================+
 #*/
-
-
-
-
-
-
-
 # +------- +------------------ +----------------------------------------------------------- # ------------+ ------------------- # --------------+
 
-    nMaxDepth=1; i=-1;                                                                      ## .(60506.01.1)
+    aVdt="Dec 03, 2022  1:31p"; aVtitle="formR gitR Tools"                                                                      # .(21113.05.6 RAM Add aVtitle for Version in Begin)
+    aVer="$( echo $0 | awk '{  match( $0, /_[dpstuv][0-9]+\.[0-9]+/ ); print substr( $0, RSTART+1, RLENGTH-1) }' )"             # .(21031.01.1 RAM Add [d...).(20416.03.8 "_p2.02", or _d1.09)
+
+    nMaxDepth=1; i=-1;                                                                                                          ## .(60506.01.1)
 #hile [[ $# > 1 ]]; do key="$1"; # echo "key: '${key}', \$2: '$2'"
 while [[ $# > 0 ]]; do key="$1"; # echo "key: '${key}', \$2: '$2'"
-                                                                                       aExcl="node_mod|bower_comp|.git"         # .(80118.01.1)
+                                                                                       aExcl="node_mod|bower_comp|\\.git"       # .(80118.01.1).(21203.01.1 RAM Exclude only ".git")
                                                                                        aIncl=""                                 # .(11010.01.1)
    case $key in
-       -r|-R)  if [ "${2:0:1}" != "-" ] && [ ! -z $2 ]; then nMaxDepth=$2; shift; else nMaxDepth=99;                 fi; ;; # echo "nMaxDepth: $nMaxDepth"; ;;
-       -d|-D)  if [ "${2:0:1}" != "-" ] && [ ! -z $2 ]; then nDays=$2;                       shift;                  fi; ;; # echo "nDays: ${nDays}"; ;;
-       -m|-M)  if [ "${2:0:1}" != "-" ] && [ ! -z $2 ]; then nMths=$2; nDays=$[nMths * 30];  shift;                  fi; ;; # echo "nDays: ${nDays}"; ;;
-       -s|-S)  if [ "${2:0:1}" != "-" ] && [ ! -z $2 ]; then nSort="$2";   shift; else nSort="2";                    fi; ;; # echo "nSort: ${nSort}"; ;;
-       -x|-X)  if [ "${2:0:1}" != "-" ] && [ ! -z $2 ]; then aExcl="$2";   shift; else aExcl="node_mod|bower_comp";  fi; ;; # echo "aExcl: ${aExcl}"; ;;
-       -i|-I)  if [ "${2:0:1}" != "-" ] && [ ! -z $2 ]; then aIncl="$2";   shift; else aIncl="node_modules$";        fi; ;; # echo "aIncl: ${aIncl}"; ;;    # .(11010.01.2 RAM Add -i Inclide option)
-#      -i|-I)  if [ "${2:0:1}" != "-" ] && [ "n" == $2 ]; then aIncl="node_module$";         fi; ;; # echo "aIncl: ${aIncl}"; ;;    # .(11010.01.2 RAM Add -i Inclide option)
-       -f|-F)  if [ "${2:0:1}" != "-" ]               ; then bFile="1" ;                     shift;                  fi; ;; # echo "bFile: ${bFile}"; ;;
-       -h|--help|-help|/\?)                                  aHelp=true;             ;;                                                                     # .(90401.01.1 Added -help)
-
-
-       *)                         i=`expr $i + 1`;           mArgs[$i]="$key";       ;;     # .(60506.01.2)
+      -r|-R)  if [ "${2:0:1}" != "-" ] && [  ! -z  $2 ]; then nMaxDepth=$2; shift; else nMaxDepth=99;                    fi; ;; # echo "nMaxDepth: $nMaxDepth"; ;;
+      -d|-D)  if [ "${2:0:1}" != "-" ] && [  ! -z  $2 ]; then nDays=$2;                       shift;                     fi; ;; # echo "nDays: ${nDays}"; ;;
+      -m|-M)  if [ "${2:0:1}" != "-" ] && [  ! -z  $2 ]; then nMths=$2; nDays=$[nMths * 30];  shift;                     fi; ;; # echo "nDays: ${nDays}"; ;;
+      -s|-S)  if [ "${2:0:1}" != "-" ] && [  ! -z  $2 ]; then nSort="$2";   shift; else nSort="2";                       fi; ;; # echo "nSort: ${nSort}"; ;;
+      -x|-X)  if [ "${2:0:1}" != "-" ] && [  ! -z  $2 ]; then aExcl="$2";   shift; else aExcl="node_mod|bower_comp";     fi; ;; # echo "aExcl: ${aExcl}"; ;;
+#     -i|-I)  if [ "${2:0:1}" != "-" ] && [  ! -z  $2 ]; then aIncl="$2";   shift; else aIncl="node_modules$";           fi; ;; # echo "aIncl: ${aIncl}"; ;; # .(11010.01.2 RAM Add -i Inclide option).(11010.01.2 RAM Use -x)
+#     -i|-I)  if [ "${2:0:1}" != "-" ] && [ "n" == $2 ]; then aIncl="node_module$";         fi; ;; # echo "aIncl: ${aIncl}"; ;;                              # .(11010.01.2 RAM Add -i Inclide option)
+      -f|-F)  if [ "${2:0:1}" != "-" ]                 ; then bFile="1" ;                     shift;                     fi; ;; # echo "bFile: ${bFile}"; ;;
+      -h|--help|-help|help|/\?)                               aHelp=true;             ;;                                                                     # .(90401.01.1 Added -help)
+      *)                         i=`expr $i + 1`;             mArgs[$i]="$key";       ;;     # .(60506.01.2)
     esac;
     shift;
    done
 # +------- +------------------ +----------------------------------------------------------- # ------------+ ------------------- # --------------+
 
-#             if [ "${1:0:1}" != "-" ];                then mArgs[i++]="$1"; fi
+#       aLstSp="echo "; if [ "${aOSv/w}" != "${aOSv}" ]; then aLstSp=""; fi                                                     # .(10706.09.1 RAM Windows returns an extra blank line).(21113.06.1 RAM Reverse).(21120.02.2)
+
+#  if [ "${1:0:1}" != "-" ]; then mArgs[i++]="$1"; fi
 
 # a="."; b='*'; opt="-maxdepth 1"
 # echo "mArgs: '${mArgs[*]}', len=${#mArgs[*]}"
 
 #  if [ ${#mArgs[*]} = 1 ];   then if [ ! -d  ${mArgs[0]}  ]; then   ##.(60910.01.1 if 1st arg is a folder, use it)
    if [ ${#mArgs[*]} = 1 ];   then if [ ! -d "${mArgs[0]}" ]; then   # .(70930.01.1 opps)
-       mArgs[1]=${mArgs[0]};  mArgs[0]="."; fi; fi
+          mArgs[1]=${mArgs[0]};  mArgs[0]="."; fi; fi
 
        opt="-maxdepth ${nMaxDepth}"; aNum=""
   if [   -z "${mArgs[0]}" ]; then aDir="."; else aDir="${mArgs[0]}";     fi
@@ -98,35 +104,33 @@ while [[ $# > 0 ]]; do key="$1"; # echo "key: '${key}', \$2: '$2'"
 # +------- +------------------ +----------------------------------------------------------- # ------------+ ------------------- # --------------+
 
   if [ ! -z "${aHelp}"    ]; then
-  echo ""
-  echo " Syntax: dir [Path] [Opts] [SearchPattern]"
-  echo "    Path:            Optional, defaults to '.'"
-  echo "    Opts: -r [n]     Search [n] directory levels, defaults to 99"
-  echo "          -d [n]     Search files saved in last [n] days, defaults to 1"
-  echo "          -m [n]     Search files saved in last [n] months, defaults to 1"
-  echo "          -s [n]     Sort output 1)Size, 2)Date & Time, 3)[path]/Filename"
-  echo "          -s [n[r]]  Reverse sort order, defaults to 2r"
-  echo "          -x [str]   Exclude RegEx pattern from result, defaults to 'node_mod|bower_comp'"
-  echo "          -i [str]   Include RegEx pattern from result, defaults to 'node_mod|bower_comp'"                              # .(11010.01.3)
-  echo "    SearchPattern:   Unix Find -iname search string, defaults to '*'"
-  echo ""
-    else
 
+    echo ""
+    echo "  File Lister         (${aVer})                                 (${aVdt})"
+    echo "  -----------------   -------------------------------------------------------------------------"
+    echo "  Syntax: RDir [Path] [Opts] {SearchPattern}"
+    echo ""
+    echo "    SearchPattern:    Unix Find -iname search string, defaults to '*'"
+    echo "    Path:             Optional, defaults to '.'"
+    echo ""
+    echo "    Opts: -r [n]      Search [n] directory levels, defaults to 99"
+    echo "          -d [n]      Search files saved in last [n] days, defaults to 1"
+    echo "          -m [n]      Search files saved in last [n] months, defaults to 1"
+    echo "          -s [n]      Sort output 1)Size, 2)Date & Time, 3)[path]/Filename"
+    echo "          -s [n[r]]   Reverse sort order, defaults to 2r"
+    echo "          -x [str]    Exclude RegEx pattern from result, defaults to 'node_mod|bower_comp'"
+#   echo "          -i [str]    Include RegEx pattern from result, defaults to 'node_mod|bower_comp'"                              # .(11010.01.3)
+#   echo ""
+  else
 
-
-
-# echo ""                                                                                                                       ##.(21114.07.2)
-# echo "        find \"${aDir}\"" ${opt} -iname "'"${aStr}"'"
-# echo "        find \"${aDir}\" ${opt} ${aSearch}${aSort}${aExcl}"                                                             ##.(11010.01.5)
+#   echo ""                                                                                                                       ##.(21114.07.2)
+#   echo "        find \"${aDir}\"" ${opt} -iname "'"${aStr}"'"
+#   echo "        find \"${aDir}\" ${opt} ${aSearch}${aSort}${aExcl}"                                                             ##.(11010.01.5)
 # if [ "${bDebug}" == "1" ]; then                                                                                               # .(21117.02.1)
-# echo "        find \"${aDir}\" ${opt} ${aSearch}${aSort}${aExcl}${aIncl}"; fi                                                 # .(11010.01.5).(21114.07.2).(21117.02.2)
-# bQuiet=0; bSpace=0; sayMsg "                                find \"${aDir}\" ${opt} ${aSearch}${aSort}${aExcl}${aIncl}" -1    # .(21117.02.2)
-# echo -e "\n Folder Size     Files    Dirs  $( pwd )/$aDir"                                                                    # .(21114.07.2)
-  echo -e "\n   File Size     Date    Time   $( pwd )/$aDir"                                                                    # .(21114.07.2)
-
-
-
-
+#   echo "        find \"${aDir}\" ${opt} ${aSearch}${aSort}${aExcl}${aIncl}"; fi                                                 # .(11010.01.5).(21114.07.2).(21117.02.2)
+#   bQuiet=0; bSpace=0; sayMsg "                                find \"${aDir}\" ${opt} ${aSearch}${aSort}${aExcl}${aIncl}" -1    # .(21117.02.2)
+#   echo -e "\n Folder Size     Files    Dirs  $( pwd )/$aDir"                                                                    # .(21114.07.2)
+    echo -e "\n   File Size     Date    Time   $( pwd )/$aDir"                                                                    # .(21114.07.2)
 
   if [ ! -z "${bFile}" ]; then
   echo "  ----------  ----------------  -------------------------------------------------------------------------------------------------------------  ------------------"
@@ -135,7 +139,6 @@ while [[ $# > 0 ]]; do key="$1"; # echo "key: '${key}', \$2: '$2'"
     else
         # +------- +------------------ +----------------------------------------------------------- # ------------+ ------------------- # --------------+
   echo "  ----------  ----------------  ---------------------------------------------------------------------------"
-
 
 #         aFmt='  %10s  %TY-%Tm-%Td %TH:%TM      %p\n';                                       #  echo "aFmt '${aFmt}'"          ##.(10707.07.1)
           aFmt='  %10s  %TY-%Tm-%Td %TH:%TM.%TS  %p\n';                                       #  echo "aFmt '${aFmt}'"          # .(10707.07.1 RAM add Seconds)
@@ -149,8 +152,9 @@ if [ "${aIncl}" == "" ]; then                                                   
 else                                                                                                                            # .(11010.01.7 Beg)
   aCmd="find \"${aDir}\" ${opt} ${aSearch} -printf \""${aFmt}"\"";
   fi
-                                                                                                                            # .(11010.01.7 End)
-# awk '{ n=index($0"/!_", "/!_"); printf "%-100s %s\n", substr($0,1,n-1), substr($0,n+1) }'                                    # .(10923.01.1 RAM ??)
+                                                                                                                                # .(11010.01.7 End)
+# awk '{ n=index($0"/!_", "/!_"); printf "%-100s %s\n", substr($0,1,n-1), substr($0,n+1) }'                                     # .(10923.01.1 RAM ??)
+# echo "${aCmd}${aExclude}${aSort}"; exit
 
 if [ "${aIncl}" == "" ]; then                                                                                                   # .(11010.01.8)
 # echo "${aCmd}${aExclude}${aSort}";  exit
@@ -169,7 +173,7 @@ if [ "$?" != "0" ]; then                                                        
 #  echo ""
    fi                                                                                                                           # .(90401.01.1 RAM Beg ??)
 
-   echo ""
+   ${aLstSp} # echo ""
    fi
 # +------- +------------------ +----------------------------------------------------------- # ------------+ ------------------- # --------------+
 #*\
