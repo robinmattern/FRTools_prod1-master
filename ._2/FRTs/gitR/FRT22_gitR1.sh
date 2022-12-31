203d757b4992f22fd682d0b37fb8d12a57d40d35
robinmattern <robin.mattern@gmail.com>
2022-12-31 15:37:01 -0500

    22131.01 Major changes to FRT22_gitR_p2.05 .(21127.03 to 21206.05)

--- ._2/FRTs/gitR/FRT22_gitR1.sh
+++ ._2/FRTs/gitR/FRT22_gitR1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #  gitR Launcher (Prod Copy)
 
-        FRT_Scr=FRT22_gitR1_p2.04.sh
+        FRT_Scr=FRT22_gitR1_p2.05.sh
 
         FRT_Dir=$( dirname "$0" )
         FRT1_Main1=$FRT_Dir/$FRT_Scr
